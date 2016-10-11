1852f867a1ced5e86b8094236f0e260109b9eb03
Provo17 <provo17.gp@gmail.com>
2016-10-11 16:08:56 -0600

    Configured routing and states for index.html, landing.html, and album.html in app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,24 @@
-angular.module('blocJams', []);
+(function(){
+    function config($stateProvider, $locationprovider) {
+        $locationprovider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+        });
+
+        $stateProvider
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+        })
+         .state('album', {
+             url: '/album',
+             templateUrl: '/templates/album.html'
+         });
+    }
+
+    angular.module('bloc-jams', ['ui.router']);
+        angular
+            .module('bloc-jams', ['ui.router'])
+            .config(config);
+})();
