4f0d0380aef6ef05f99eca45c5c22c734ac2f482
Provo17 <provo17.gp@gmail.com>
2016-10-17 17:29:05 -0600

    Added getCollection method to fixture service. *minor bug fixes*

--- public/scripts/services/Fixtures.js
+++ public/scripts/services/Fixtures.js
@@ -1,21 +1,21 @@
  (function() {
      function Fixtures() {
-         var Fixtures = {};
+        var Fixtures = {};
 
         var albumPicasso = {
          title: 'The Colors',
          artist: 'Pablo Picasso',
          label: 'Cubism',
          year: '1881',
-         albumArtUrl: '/assets/images/album_covers/01.png',
+         albumArtUrl: 'assets/images/album_covers/01.png',
          songs: [
              { title: 'Blue', duration: '161.71', audioUrl: '/assets/music/blue' },
              { title: 'Green', duration: '103.96', audioUrl: '/assets/music/green' },
              { title: 'Red', duration: '268.45', audioUrl: '/assets/music/red' },
              { title: 'Pink', duration: '153.14', audioUrl: '/assets/music/pink' },
              { title: 'Magenta', duration: '374.22', audioUrl: '/assets/music/magenta' }
-         ]
-     };
+            ]
+        };
 
         var albumMarconi = {
          title: 'The Telephone',
@@ -29,12 +29,20 @@
              { title: 'Fits in your pocket', duration: '3:21' },
              { title: 'Can you hear me now?', duration: '3:14' },
              { title: 'Wrong phone number', duration: '2:15' }
-         ]
-     };
+            ]
+        };
 
         Fixtures.getAlbum = function() {
-         return albumPicasso;
-     };
+            return albumPicasso;
+        };
+
+        Fixtures.getCollection = function(numberOfAlbums) {
+             var albums = [];
+             for (var i = 0; i < numberOfAlbums; i++) {
+                 albums.push(albumPicasso);
+             }
+             return albums;
+         };
 
          return Fixtures;
      }
